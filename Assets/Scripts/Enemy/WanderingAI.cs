f3165d042a90937b438070b477be7736018cf26a
UnaSwims22 <maswimeunarine@gmail.com>
2025-08-18 01:27:59 +0200

    ai shoots projectiles

--- Assets/Scripts/Enemy/WanderingAI.cs
+++ Assets/Scripts/Enemy/WanderingAI.cs
@@ -1,7 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
 
 public class WanderingAI : MonoBehaviour
 {
+    [SerializeField] 
+    private GameObject fireballPrefab;
+    private GameObject _fireball;
+ 
     public float speed = 3.0f;  // movement speed of AI
     public float obstacleRange = 5.0f; //reaction range
 
@@ -28,6 +34,25 @@ public class WanderingAI : MonoBehaviour
         if (_alive)
         {
             transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        if (Physics.SphereCast(ray, 0.75f, out hit))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+
+            if (hitObject.GetComponent<PlayerCharacter>())
+            {
+                if (_fireball == null)
+                {
+                    _fireball = Instantiate(fireballPrefab) as GameObject;
+                    _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                }
+            }
+            else if (hit.distance < obstacleRange)
+            {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
         }
     }
     public void SetAlive(bool alive)
