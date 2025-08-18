17f335440954d881fab39d6a9984316f9c113982
UnaSwims22 <maswimeunarine@gmail.com>
2025-08-18 02:40:02 +0200

    game edits

--- Assets/Scripts/Player/PlayerCharacter.cs
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int _health;
+    private bool _isDead = false;    //player will only "die" once
+
+    [Header("Game Over UI")]
+    public GameObject gameOverUI;
+
     void Start()
     {
-        
+        _health = 5;
+        if (gameOverUI != null)
+            gameOverUI.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hurt (int damage)   //health decreases
     {
-        
+        if (_isDead) return;
+
+        _health -= damage;
+        Debug.Log("Health: " + _health);
+
+        if (_health <=0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (_isDead) return;
+
+        _isDead = true;
+        Debug.Log("Player has died!");
+
+        Destroy(gameObject);
+
+        var controller = GetComponent<FPController>();
+        if (controller != null)
+            controller.enabled = false;
+
+        if (gameOverUI != null)
+            gameOverUI.SetActive(true);
+
     }
 }
