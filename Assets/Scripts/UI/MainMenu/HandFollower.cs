48963bd44e3c943a6af887a70d83785c2887a82f
UnaSwims22 <maswimeunarine@gmail.com>
2025-10-18 01:46:13 +0200

    menu edit

--- Assets/Scripts/UI/MainMenu/HandFollower.cs
+++ Assets/Scripts/UI/MainMenu/HandFollower.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HandFollower : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public MenuButtonController menuButtonController;
+    public RectTransform[] buttonPositions; //Assign each buttons RectTransform
+    public float moveSpeed = 5f;
+    public Vector3 offset = new Vector3(-150f, 0, 0);
+
+    private Vector3 startPosition;
+    private bool hasStartedFollowing = false;
+
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (buttonPositions.Length == 0  ||  menuButtonController == null) return;
+
+        int targetIndex = menuButtonController.index;
+
+        Vector3 targetPos = buttonPositions[targetIndex].position + offset;
+
+        if (!hasStartedFollowing)
+        {
+            hasStartedFollowing = true;
+            transform.position = startPosition;
+        }
         
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * moveSpeed);
     }
 }
