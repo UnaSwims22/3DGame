870fde7cee8509e67af3612e44fa933e455bcaec
UnaSwims22 <maswimeunarine@gmail.com>
2025-11-07 02:22:34 +0200

    added compass scripts

---
+++ Assets/Scripts/UI/HUD/Compass.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public Transform player;
+    Vector3 dir;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir.z = player.eulerAngles.y;
+        transform.localEulerAngles = dir;
+    }
+}
