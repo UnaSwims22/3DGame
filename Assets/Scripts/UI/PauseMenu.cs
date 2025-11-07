185f3febe130a038d8001fb752874defe1bc526f
UnaSwims22 <maswimeunarine@gmail.com>
2025-11-07 19:18:18 +0200

    making edits to controls scene

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -6,6 +6,7 @@ public class PauseMenu : MonoBehaviour
 {
     [Header("Scene Names")]
     [SerializeField] string controlsSceneName = "Controls";
+    [SerializeField] string mainMenuSceneName = "Main Menu";
     
     public GameObject pauseMenuUI;
     public GameObject resume;
@@ -14,6 +15,16 @@ public class PauseMenu : MonoBehaviour
 
     private bool isPaused = false;
 
+    public static bool comingFromPauseMenu = false;
+
+     void Start()
+    {
+        if (comingFromPauseMenu)
+        {
+            comingFromPauseMenu = false;
+            Pause();
+        }
+    }
     void Update()
     {
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
@@ -22,37 +33,37 @@ public class PauseMenu : MonoBehaviour
                 Resume();
             else
                 Pause();
-                    
+
         }
-
-        if (pauseMenuUI != null)
-        {
-            pauseMenuUI.SetActive(true);
-        }
-
-
-
     }
+
     public void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;  //freezes game
-        isPaused = false;
+        isPaused = true;
     }
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;  //unfreezes game
+        isPaused = false;
     }
 
-    public void PressControlls()
+    public void PressControls()
     {
+        comingFromPauseMenu = true;
+
+        Time.timeScale = 1f;
+
         SceneManager.LoadScene(sceneName: "Controls");
     }
 
-    public void QuitButton()
+    public void QuitToMainMenu()
     {
+
+        Time.timeScale = 1f;
         SceneManager.LoadScene(sceneName: "Main Menu");
     }
 
